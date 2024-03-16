5f9b77c048c77f41f2b5b3d48301f99d89b4dfc2
Kristian Ziu <k.ziu@binnova.it>
2024-03-16 14:29:23 +0100

    Last

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ def get_links(url):
         links.append(link['href'])
     return links
 
-def get_content(url):
-    return url
+def get_text(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    html_page = urlopen(req).read()
+    soup = BeautifulSoup(html_page, 'html.parser')
+    return soup.get_text(separator='\r', strip=True)
+
+def scrape(url, current_depth, maximum_depth, link_index=0):
+
+    print(f"[{current_depth}][{link_index}]")
+    f = open(f"files/[{current_depth}][{link_index}].txt", "w", encoding="utf-8")
+    text = get_text(url)
+    f.write(text)
+    f.close()
+
+    if current_depth == maximum_depth:
+        return
+
+    links = get_links(url)
+    for link_index, link in enumerate(links):
+        scrape(link, current_depth + 1, maximum_depth, link_index)
 
 
-
-print(get_links('https://stackoverflow.com/questions/53911695/scrape-urls-using-beautifulsoup-in-python-3'))
+scrape('https://stackoverflow.com/questions/53911695/scrape-urls-using-beautifulsoup-in-python-3', 0, 1)
